f8eab73ec52a77e28deb3f3b1b8a1dded742860f
TiyaZhan <tizhan@ucsd.edu>
2022-10-12 14:47:43 -0700

    Fixed all reversed methods in arrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,34 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedSize2()
+  {
+    int[] input = {2, 3};
+    assertArrayEquals(new int[] {3,2}, ArrayExamples.reversed(input));
+  }
+
+  @Test
+  public void testReversedSize1()
+  {
+    int[] input = {2};
+    assertArrayEquals(new int[] {2}, ArrayExamples.reversed(input));
+  }
+
+  @Test
+  public void testReversedInPlaceEven()
+  {
+    int[] input = {2, 3};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[] {3,2}, input);
+  }
+
+  @Test
+  public void testReversedInplaceOdd()
+  {
+    int[] input = {2,3,4};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[] {4, 3, 2}, input);
+  }
 }
